9122cf02352cefa1a8a6c59ac999cb54cfe76e08
rafasoyer <r.soyer@carepay.com>
2021-03-06 18:04:33 +0100

    defingin have hierarchy hands of victory

--- src/main/java/org/acme/game/GameApp.java
+++ src/main/java/org/acme/game/GameApp.java
@@ -1,16 +1,20 @@
 package org.acme.game;
 
 import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
 import java.util.Scanner;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-import static org.acme.game.GameApp.HandOptions.FINISH;
 import static org.acme.game.GameApp.HandOptions.PAPER;
 import static org.acme.game.GameApp.HandOptions.ROCK;
 import static org.acme.game.GameApp.HandOptions.SCISSORS;
+import static org.acme.game.GameApp.MatchOutcome.TIE;
+import static org.acme.game.GameApp.MatchOutcome.USER_LOST;
+import static org.acme.game.GameApp.MatchOutcome.USER_WIN;
 
 public class GameApp {
 
@@ -24,8 +28,9 @@ public class GameApp {
 
         private final Map<Integer, HandOptions> possibleOptions = Map.of(PAPER.getOption(), PAPER,
                                                                          ROCK.getOption(), ROCK,
-                                                                         SCISSORS.getOption(), SCISSORS,
-                                                                         FINISH.getOption(), FINISH);
+                                                                         SCISSORS.getOption(), SCISSORS);
+
+        private final Random random = new Random();
 
         private void run() {
             final Scanner input = new Scanner(System.in);
@@ -34,8 +39,24 @@ public class GameApp {
             do {
                 System.out.println("Show your hand \n\tYour Options: " + HandOptions.options());
                 handSymbol = input.nextInt();
+                userOption(handSymbol);
+                machineOption();
                 System.out.println("You entered string " + handSymbol);
             } while (handSymbol != 0);
+        }
+
+        private HandOptions userOption(final int hand) {
+            return Optional
+                     .ofNullable(possibleOptions.get(hand))
+                     .orElseThrow(() -> new IllegalArgumentException("Invalid option"));
+        }
+
+        @SuppressWarnings("OptionalGetWithoutIsPresent")
+        private HandOptions machineOption() {
+            return possibleOptions.get(random
+                                         .ints(1, 3)
+                                         .findFirst()
+                                         .getAsInt());
         }
     }
 
@@ -43,18 +64,51 @@ public class GameApp {
     @ToString
     @RequiredArgsConstructor
     enum HandOptions {
-        PAPER(1),
-        ROCK(2),
-        SCISSORS(3),
-        FINISH(0);
+        PAPER(1) {
+            @Override
+            MatchOutcome beat(final HandOptions against) {
+                return switch (against) {
+                    case PAPER -> TIE;
+                    case SCISSORS -> USER_LOST;
+                    case ROCK -> USER_WIN;
+                };
+            }
+        },
+        ROCK(2) {
+            @Override
+            MatchOutcome beat(final HandOptions against) {
+                return switch (against) {
+                    case PAPER -> USER_LOST;
+                    case SCISSORS -> USER_WIN;
+                    case ROCK -> TIE;
+                };
+            }
+        },
+        SCISSORS(3) {
+            @Override
+            MatchOutcome beat(final HandOptions against) {
+                return switch (against) {
+                    case PAPER -> USER_WIN;
+                    case SCISSORS -> TIE;
+                    case ROCK -> USER_LOST;
+                };
+            }
+        };
 
         private final int option;
+
+        abstract MatchOutcome beat(final HandOptions against);
 
         public static String options() {
             return PAPER.getOption() + ". " + PAPER.name() + "\n\t\t\t\t"
                      + ROCK.getOption() + ". " + ROCK.name() + "\n\t\t\t\t"
-                     + SCISSORS.getOption() + ". " + SCISSORS.name() + "\n\t\t\t\t"
-                     + FINISH.getOption() + ". " + FINISH.name();
+                     + SCISSORS.getOption() + ". " + SCISSORS.name() + "\n\t\t\t\t";
         }
+    }
+
+    enum MatchOutcome {
+        USER_WIN,
+        USER_LOST,
+        TIE;
     }
 }
