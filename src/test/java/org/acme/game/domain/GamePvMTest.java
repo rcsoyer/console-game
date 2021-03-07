41ff5f6c3276aad2dae1774c95371ff6b3101413
rafasoyer <r.soyer@carepay.com>
2021-03-07 15:22:11 +0100

    refactoring

---
+++ src/test/java/org/acme/game/domain/GamePvMTest.java
@@ -0,0 +1,17 @@
+package org.acme.game.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GamePvMTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void start() {
+    }
+}
