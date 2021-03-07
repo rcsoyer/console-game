2586e7b7ce6faf6eb5610a0464179a307ac03a59
rafasoyer <r.soyer@carepay.com>
2021-03-07 15:04:42 +0100

    refactoring

--- src/main/java/org/acme/game/domain/MachineHand.java
+++ src/main/java/org/acme/game/domain/MachineHand.java
@@ -2,13 +2,16 @@ package org.acme.game.domain;
 
 import java.util.Random;
 
+import static org.acme.game.domain.Hand.PAPER;
+import static org.acme.game.domain.Hand.SCISSORS;
+
 final class MachineHand {
 
     private final Random random = new Random();
 
     @SuppressWarnings("OptionalGetWithoutIsPresent")
     Hand random() {
-        return Hand.parse(random.ints(1, 4)
+        return Hand.parse(random.ints(PAPER.getHand(), SCISSORS.getHand() + 1)
                                 .findFirst()
                                 .getAsInt());
     }
