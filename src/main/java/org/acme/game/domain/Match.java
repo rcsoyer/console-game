3991cf8a6ad8cbf03763cf8baed6c199ff2c132f
rafasoyer <r.soyer@carepay.com>
2021-03-07 21:49:52 +0100

    adding javadocs

--- src/main/java/org/acme/game/domain/Match.java
+++ src/main/java/org/acme/game/domain/Match.java
@@ -1,9 +1,15 @@
 package org.acme.game.domain;
 
+/**
+ * Each match of the game can use it to define the outcome
+ */
 final class Match {
 
     private final MachineHand machine = new MachineHand();
 
+    /**
+     * Given the informed user hand and a random machine hand defines calculates the outcome
+     */
     MatchResult showHands(final int userOption) {
         final Hand machineHand = machine.random();
         final Hand userHand = Hand.parse(userOption);
@@ -11,9 +17,15 @@ final class Match {
         return new MatchResult(userHand, machineHand, outcome);
     }
 
+    /**
+     * Data of what was played in a match
+     */
     static record MatchResult(Hand userHand, Hand machineHand, MatchOutcome outcome) {
     }
 
+    /**
+     * All possible outcomes for a game between 2 hands on rock-paper-scissors
+     */
     enum MatchOutcome {
         USER_WIN, USER_LOST, TIE
     }
