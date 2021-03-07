4f6977716cb977bc403f4471cd1da63cfb4af379
rafasoyer <r.soyer@carepay.com>
2021-03-07 21:46:11 +0100

    adding javadocs

--- src/main/java/org/acme/game/domain/MachineHand.java
+++ src/main/java/org/acme/game/domain/MachineHand.java
@@ -5,10 +5,18 @@ import java.util.Random;
 import static org.acme.game.domain.Hand.PAPER;
 import static org.acme.game.domain.Hand.SCISSORS;
 
+/**
+ * Holds the logic the defines the hand played by the machine
+ */
 final class MachineHand {
 
     private final Random random = new Random();
 
+    /**
+     * A random hand defined at runtime
+     *
+     * @implNote Completely random. No logic in predicting over past moves or current move of the player
+     */
     @SuppressWarnings("OptionalGetWithoutIsPresent")
     Hand random() {
         return Hand.parse(random.ints(PAPER.getHand(), SCISSORS.getHand() + 1)
