85bdb80a2dd105810ac2c752cbc877eb576d2895
rafasoyer <r.soyer@carepay.com>
2021-03-07 21:38:09 +0100

    adding javadocs

--- src/main/java/org/acme/game/domain/GameScore.java
+++ src/main/java/org/acme/game/domain/GameScore.java
@@ -10,10 +10,16 @@ import static java.util.Map.Entry.comparingByValue;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
+/**
+ * Holds the game session score per each match
+ */
 final class GameScore {
 
     private final List<MatchResult> results = new LinkedList<>();
 
+    /**
+     * Adds results of each match
+     */
     void addMatchResult(final MatchResult result) {
         System.out.println("Match result: " + result.outcome().name());
         System.out.println("You played: " + result.userHand().name());
@@ -21,6 +27,9 @@ final class GameScore {
         results.add(result);
     }
 
+    /**
+     * Print final result of the gaming session from accumulated matches
+     */
     void showScore() {
         System.out.println("Endgame\n");
         System.out.println("Session Game Score: ");
