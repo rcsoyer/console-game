8cae412a497c97168130130ace0ecc2cb24c2265
rafasoyer <r.soyer@carepay.com>
2021-03-07 14:41:08 +0100

    unit tests

--- src/test/java/org/acme/game/domain/HandTest.java
+++ src/test/java/org/acme/game/domain/HandTest.java
@@ -14,9 +14,24 @@ class HandTest {
     }
 
     @Test
-    void showDown_HAND() {
+    void showDown_PAPER() {
+        assertEquals(MatchOutcome.TIE, PAPER.showDown(PAPER));
+        assertEquals(MatchOutcome.USER_WIN, PAPER.showDown(ROCK));
+        assertEquals(MatchOutcome.USER_LOST, PAPER.showDown(SCISSORS));
+    }
+
+    @Test
+    void showDown_ROCK() {
         assertEquals(MatchOutcome.TIE, ROCK.showDown(ROCK));
         assertEquals(MatchOutcome.USER_LOST, ROCK.showDown(PAPER));
         assertEquals(MatchOutcome.USER_WIN, ROCK.showDown(SCISSORS));
     }
+
+    @Test
+    void showDown_SCISSORS() {
+        assertEquals(MatchOutcome.USER_LOST, SCISSORS.showDown(ROCK));
+        assertEquals(MatchOutcome.USER_WIN, SCISSORS.showDown(PAPER));
+        assertEquals(MatchOutcome.TIE, SCISSORS.showDown(SCISSORS));
+    }
+
 }
